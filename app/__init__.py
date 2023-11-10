8a5daa6860434558700cca4d1907abbfdbb4f5b2
paperboi <jeffreysamjacob@gmail.com>
2023-11-10 16:29:59 +0530

    removed file handler

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,5 @@ from flask import Flask
 from logging import FileHandler,WARNING
 
 app = Flask(__name__, template_folder="templates")
-file_handler = FileHandler('errorlog.txt')
-file_handler.setLevel(WARNING)
 
 from app import routes
